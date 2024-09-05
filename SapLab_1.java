a0c08a7a0235156fdef71e18d23c01e96a01fb7f
Rahul Maurya <78677484+k26rahul@users.noreply.github.com>
2024-09-05 21:52:53 +0530

    Java-Cheat-Sheet.ipynb

---
+++ SapLab_1.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class SapLab_1 {
+  // Find max values of each row
+  public static int[] solve(int[][] matrix) {
+    int[] answers = new int[matrix.length]; // Array for max values
+
+    for (int i = 0; i < matrix.length; i++) {
+      int maxInRow = Integer.MIN_VALUE; // Initial max value
+      for (int j = 0; j < matrix[i].length; j++) {
+        if (matrix[i][j] > maxInRow) {
+          maxInRow = matrix[i][j]; // Update max value
+        }
+      }
+      answers[i] = maxInRow; // Store max for row
+    }
+
+    return answers;
+  }
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    int N = scanner.nextInt(); // Read rows
+    int M = scanner.nextInt(); // Read columns
+
+    int[][] matrix = new int[N][M]; // Matrix array
+
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < M; j++) {
+        matrix[i][j] = scanner.nextInt(); // Read matrix elements
+      }
+    }
+
+    int[] result = solve(matrix); // Get max values
+
+    for (int value : result) {
+      System.out.println(value); // Print max values
+    }
+
+    scanner.close(); // Close scanner
+  }
+}
---
+++ SapLab_2.java
@@ -0,0 +1,3 @@
+public class SapLab_2 {
+  public static void main(String[] args) {}
+}
